7811d924c2ec099e5dfbfdccd82ea826098dadee
kmcabano <kmcabano@gmail.com>
2021-11-27 17:03:44 -0500

    create basic trip model

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class Trip(models.Model):
   name = models.CharField(max_length=100)
+  start = models.DateField()
+  end = models.DateField()
+  description = models.TextField(max_length=250)
 
   def __str__(self):
     return self.name
