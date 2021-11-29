016af6a6c0a3ac62ddb37b7bcb32507cabe4e877
kmcabano <kmcabano@gmail.com>
2021-11-29 11:54:44 -0500

    create stop model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Trip
+from .models import Trip, Stop
 
 # Register your models here.
 admin.site.register(Trip)
+admin.site.register(Stop)
--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,14 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
 
-# Create your models here.
+POSITIONS = (
+  ('O', 'Origin'),
+  ('S', "Stop"),
+  ('D', 'Destination')
+)
+
 class Trip(models.Model):
   name = models.CharField(max_length=100)
   start = models.DateField()
@@ -11,3 +17,22 @@ class Trip(models.Model):
 
   def __str__(self):
     return self.name
+
+class Stop(models.Model):
+  name = models.CharField(max_length=100)
+  arrive = models.DateField()
+  depart = models.DateField()
+  position = models.CharField(
+    max_length=1,
+    choices=POSITIONS,
+    default = POSITIONS[1][1]
+  )
+  lodging = models.CharField(max_length=100)
+  food = ArrayField(models.CharField(max_length=200))
+  todos = ArrayField(models.CharField(max_length=200))
+
+  trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+  # def __str__(self):
+  #     return f"{self.get_position_display()}"
+  
